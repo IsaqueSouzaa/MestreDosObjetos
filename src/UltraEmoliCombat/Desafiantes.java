6abacb9873066df7a6ec87cc5eda131f6dd7efae
IsaqueSouzaa <isaquebarbosa463@gmail.com>
2025-09-10 00:04:07 -0300

    Resolvi o primeiro exercicio de POO

--- src/UltraEmoliCombat/Desafiantes.java
+++ src/UltraEmoliCombat/Desafiantes.java
@@ -19,7 +19,9 @@ public class Desafiantes {
 
                l[5] = new Lutador("Nerdaart","EUA", 30,1.81f,105.7f,12,2,4);
 
-        l[0].status();
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(l[0], l[1]);
+        UEC01.lutar();
 
     }
 }
