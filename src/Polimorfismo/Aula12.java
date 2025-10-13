20cbb750f20898146f64a3d7b2e5a34bff7d8f63
IsaqueSouzaa <isaquebarbosa463@gmail.com>
2025-10-13 01:29:51 -0300

    Aprendi conceitos de polimorfismo em sobreposicao

---
+++ src/Polimorfismo/Aula12.java
@@ -0,0 +1,28 @@
+package Polimorfismo;
+
+public class Aula12 {
+    public static void main(String[] args) {
+
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new Ave();
+
+        m.setPeso(35.3f);
+        m.setCorPelo("Marrom");
+        m.alimentar();
+        m.locomover();
+        m.emitirSom();
+
+        Canguru c = new Canguru();
+        Cachorro k = new Cachorro();
+        Cobra j = new Cobra();
+        Tartaruga t = new Tartaruga();
+        Goldfish g = new Goldfish();
+        Arara e = new Arara();
+
+        c.locomover();
+
+
+    }
+}
