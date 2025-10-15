89fdb2e9a1600172467ef6d8bce785e13c81f80c
IsaqueSouzaa <isaquebarbosa463@gmail.com>
2025-10-15 00:21:20 -0300

    Pratiquei os conhecimentos de polimorfimo por sobrecarga em Polimorfismo2

---
+++ src/Polimorfismo/Polimorfismo2/Aula13.java
@@ -0,0 +1,13 @@
+package Polimorfismo.Polimorfismo2;
+
+public class Aula13 {
+
+    public static void main(String[] args) {
+
+        Cachorro c = new Cachorro();
+
+        c.reagir(true);
+        c.reagir(false);
+
+    }
+}
